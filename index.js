270ec92f3e8b232d123a922c65bc749937470991
Scott Hill <scott.c.hill.jr@gmail.com>
2022-03-14 17:31:10 -0500

    deletes index html and index js to redo them

--- index.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const plantContainer = document.getElementById('plant-container')
-    const backButton = document.getElementById('back')
-    const forwardButton = document.getElementById('forward')
-    const formContainer = document.getElementById('create-plant')
