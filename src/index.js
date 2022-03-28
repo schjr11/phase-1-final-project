b2fe3817d5a641a8de8eab380bfd0c92dea2167b
Scott Hill <scott.c.hill.jr@gmail.com>
2022-03-27 19:32:42 -0500

    finishes requests for deleting and patching from fetch

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-let addPlant = false
+let showForm = false
 
 document.addEventListener("DOMContentLoaded", () => {
   getPlants();
   const addBtn = document.querySelector("#new-plant-btn")
   const plantForm = document.querySelector('.container')
   addBtn.addEventListener('click', () =>{
-    addPlant = !addPlant
-    if (addPlant) {
+    if(showForm === false){
+      showForm = true;
       plantForm.style.display = 'block'
-    } else {
+    } else if(showForm === true){
+      showForm = false
       plantForm.style.display = 'none'
     }
   })
@@ -16,48 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
     e.preventDefault();
     postPlant(e.target.name.value, e.target.image.value)
   })
-  // const deleteBtn = document.querySelector("#delete-btn")
-  // deleteBtn.addEventListener('click', () =>{
-  //   console.log("test")
-  // })
 })
-
-function getPlants() {
-  fetch ('http://localhost:3000/plants/')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    data.map((p) => {
-      renderPlant(p)
-      test()
-    })
-  })
-
-}
-
-function renderPlant(p) {
-  const plantCard =`<div class="card">
-    <h2>${p.name}</h2>
-    <img src=${p.image} class="plant-avatar" />
-    <p>${p.likes} Likes </p>
-    <button class="like-btn"> Like </button>
-    <button class="delete-btn"> Delete </button>
-
-  </div>`
-
-  const plantGarden = document.getElementById('plant-collection')
-  plantGarden.innerHTML += plantCard;
-}
-
-function test(){
-  const deleteBtns = document.querySelectorAll(".delete-btn");
-  for(let i = 0; i < deleteBtns.length; i++){
-    deleteBtns[i].addEventListener('click', () => {console.log("test")})
-  }
- 
-}
-
 
 function postPlant(name, url) {
   fetch ('http://localhost:3000/plants', {
@@ -76,49 +36,68 @@ function postPlant(name, url) {
     return response.json();
   })
   .then(function(data) {
-    renderPlant(data)
+    getPlants()
   })
 }
 
+function getPlants() {
+  fetch ('http://localhost:3000/plants/')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(plants => renderPlant(plants))
 
+}
 
-// function patchPlant(newNumberOfLikes) {
-//   fetch ('http://localhost:3000/plants/:id', {
-//     method: 'PATCH',
-//     headers: {
-//       "Content-Type": "application/json",
-//       Accept: "application/json"
-//     },
-//     body: JSON.stringify({
-//       "likes": newNumberOfLikes,
-//     })
-//   })
-//   .then(function(response) {
-//     return response.json();
-//   })
-//   .then(function(data) {
-//     renderPlant(data)
-//   })
-// }
+function renderPlant(plants) {
 
-// function deletePlant(name, url) {
-//   fetch ('http://localhost:3000/plants/', {
-//     method: 'DELETE',
-//     headers: {
-//       "Content-Type": "application/json",
-//       Accept: "application/json"
-//     },
-//     body: JSON.stringify({
-//       "name": name,
-//       "image": url,
-//     })
-//   })
-//   .then(function(response) {
-//     return response.json();
-//   })
-//   .then(function(data) {
-//     renderPlant(data)
-//   })
-// }
+  plants.forEach(p => {
+    const plantCard = document.createElement("div")
+    plantCard.className = "card"
+    plantCard.id = p.id;
+    plantCard.innerHTML = `
+    <h2>${p.name}</h2>
+    <img src=${p.image} class="plant-avatar" />
+    <p>${p.likes}</p><span> Likes </span>
+    <button class="like-btn"> Like </button>
+    <button class="delete-btn"> Delete </button>`
 
-// How to make a delete button??????????
+  plantCard.addEventListener("click", (event) =>{
+      if (event.target.matches("button.like-btn")){
+        p.likes++
+        plantCard.querySelector("p").innerText = p.likes
+        increaseLikes(p)
+      }
+      if(event.target.matches("button.delete-btn")){
+        plantCard.remove()
+        deletePlant(p);
+      }
+    })
+    const plantGarden = document.getElementById('plant-collection');
+    plantGarden.appendChild(plantCard);
+
+  })
+}
+
+function increaseLikes(p){
+  fetch(`http://localhost:3000/plants/${p.id}`,{
+    method: "PATCH",
+    headers:{
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({
+      likes: p.likes
+    })
+  });
+}
+
+function deletePlant(p){
+  fetch(`http://localhost:3000/plants/${p.id}`,{
+    method: "DELETE",
+    headers:{
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    }
+  });
+}
