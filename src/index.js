c60f30ae039f7071f0db2fe145b082424c65f407
Scott Hill <scott.c.hill.jr@gmail.com>
2022-04-01 21:43:27 -0500

    adds comments to explain code in index js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+// Don't show container right away
 let showForm = false
 
+// Get plants when the page loads immediately
 document.addEventListener("DOMContentLoaded", () => {
   getPlants();
+// Click "Add a new plant!" to open the container and to close it again
   const addBtn = document.querySelector("#new-plant-btn")
   const plantForm = document.querySelector('.container')
   addBtn.addEventListener('click', () =>{
@@ -13,12 +16,15 @@ document.addEventListener("DOMContentLoaded", () => {
       plantForm.style.display = 'none'
     }
   })
+
+// Post a plant by submitting on the form
   plantForm.addEventListener('submit', (e) => {
     e.preventDefault();
     postPlant(e.target.name.value, e.target.image.value)
   })
 })
 
+// Post a new plant into the db.json and return a response
 function postPlant(name, url) {
   fetch ('http://localhost:3000/plants', {
     method: 'POST',
@@ -36,10 +42,11 @@ function postPlant(name, url) {
     return response.json();
   })
   .then(function(data) {
-    getPlants()
+    getPlants(data)
   })
 }
 
+// Render the plants that you get
 function getPlants() {
   fetch ('http://localhost:3000/plants/')
   .then(function(response) {
@@ -50,7 +57,7 @@ function getPlants() {
 }
 
 function renderPlant(plants) {
-
+// Create a card for each plant
   plants.forEach(p => {
     const plantCard = document.createElement("div")
     plantCard.className = "card"
@@ -62,6 +69,7 @@ function renderPlant(plants) {
     <button class="like-btn"> Like </button>
     <button class="delete-btn"> Delete </button>`
 
+// Create a click event to "like" a plant
   plantCard.addEventListener("click", (event) =>{
       if (event.target.matches("button.like-btn")){
         p.likes++
@@ -79,6 +87,7 @@ function renderPlant(plants) {
   })
 }
 
+// Create a patch request for "liking" a plant on a plant card
 function increaseLikes(p){
   fetch(`http://localhost:3000/plants/${p.id}`,{
     method: "PATCH",
@@ -92,6 +101,7 @@ function increaseLikes(p){
   });
 }
 
+//Create a delete request to get rid of a plant card
 function deletePlant(p){
   fetch(`http://localhost:3000/plants/${p.id}`,{
     method: "DELETE",
